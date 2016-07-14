5b56363bae459e9cfda0eb7c2abe42d73cf1c2af
IranthaJ <irantha.86@gmail.com>
2016-07-14 23:39:56 +0530

    V1.1.2.3

--- SKMN/Views/Home.xaml.cs
+++ SKMN/Views/Home.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MyServices;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,15 +22,34 @@ namespace SKMN.Views
     /// </summary>
     public partial class Home : UserControl
     {
+        string connString = "";
+        DBservices dbservice;
         public Home()
         {
             InitializeComponent();
+            dbservice = new DBservices();
+            connString = Properties.Settings.Default.AttendanceConnectionString;
+            List<DateTime> holidays = dbservice.getAllHolidays(connString);
+            AddHolidaysToCalandar(holidays);
         }
+
         public void addOnButtonClickHandler(RoutedEventHandler handler)
         {
             this.btn_attendance.Click += handler;
             this.btn_report.Click += handler;
             this.btn_settings.Click += handler;
+        }
+
+        private void AddHolidaysToCalandar(List<DateTime> holidays)
+        {
+            foreach (DateTime day in holidays)
+            {
+                myCalandar.BlackoutDates.Add(new CalendarDateRange(day,day));
+                //myCalandar.SelectedDates.Add(day);
+            }
+
+            //mycalandar.SelectedDates.Add(new DateTime(2016, 6, 15));
+
         }
 
     }
--- SKMN/Views/ReportWindow.xaml.cs
+++ SKMN/Views/ReportWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using Microsoft.Win32;
 
 namespace SKMN.Views
 {
@@ -27,6 +31,7 @@ namespace SKMN.Views
         string connString = "";
         string errorLog = "";
         DataTable recordsFromDB = new DataTable();
+        List<ReportRecord> records = new List<ReportRecord>();
         public ReportWindow()
         {
             InitializeComponent();
@@ -58,7 +63,7 @@ namespace SKMN.Views
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-
+            ReportRecord rr;
             foreach(DataRow row in recordsFromDB.Rows)
             {
                 DateTime? recordDate = row[2] is DBNull ? null : (DateTime?)row[2];
@@ -67,10 +72,119 @@ namespace SKMN.Views
 
                 DateTime? signin = row[3] is DBNull ? null : (DateTime?)row[3];
                 DateTime? signout = row[4] is DBNull ? null : (DateTime?)row[4];
-                reporter.Items.Add(new ReportRecord(Convert.ToInt32(row[1]), recordDate, signin, signout, companyStartTime, companyEndTime));
+                rr = new ReportRecord(Convert.ToInt32(row[1]), recordDate, signin, signout, companyStartTime, companyEndTime);
+                records.Add(rr);
+                reporter.Items.Add(rr);
             }
             //reporter.Items.Add(new ReportRecord(85685,new DateTime(2016,6,14),new DateTime(2016,6,14,8,2,26),new DateTime(2016,6,14,17,36,45),new DateTime(2016,6,14,8,0,0),new DateTime(2016,6,14,17,0,0)));
             //reporter.Items.Add(new ReportRecord(85685, new DateTime(2016, 6, 14), new DateTime(2016, 6, 14, 7, 52, 26), new DateTime(2016, 6, 14, 17, 10, 45), new DateTime(2016, 6, 14, 8, 0, 0), new DateTime(2016, 6, 14, 17, 0, 0)));
         }
+        PdfPTable table = new PdfPTable(11);
+        private void pdf_report_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = String.Format("{0}.pdf", reportTitle);
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    Document document = new Document(PageSize.A4, 10, 10, 15, 10);
+                    document.AddTitle("Attendance Reports");
+                    document.AddSubject("Generated report document");
+                    document.AddCreator("SKMN Fingerprint Attendance System");
+                    document.AddAuthor("Irantha Jayasekara");
+                    document.AddHeader("Nothing", "No Header");
+                    PdfWriter writer = PdfWriter.GetInstance(document, fs);
+                    document.Open();
+                    
+                    Chunk chunk = new Chunk(reportTitle,FontFactory.GetFont("Tahoma", 14, Font.BOLD));
+                    document.Add(chunk);
+
+                    //Phrase phrase = new Phrase("This is from Phrase.");
+                    //document.Add(phrase);
+
+                    iTextSharp.text.Paragraph para = new iTextSharp.text.Paragraph(DateTime.Now.ToString("dd/MM/yyyy"), FontFactory.GetFont("Tahoma", 11, Font.BOLD));
+                    document.Add(para);
+
+
+                    //actual width of table in points
+                    table.TotalWidth = 580f;
+                    //fix the absolute width of the table
+                    table.LockedWidth = true;
+
+                    //relative col widths in proportions - 1/3 and 2/3
+                    float[] widths = new float[] { 0.8f, 1.2f, 1f, 0.8f, 0.8f, 1f, 0.8f,0.8f, 1f, 1f, 1.9f };
+                    table.SetWidths(widths);
+                    table.HorizontalAlignment = 0;
+                    //leave a gap before and after the table
+                    table.SpacingBefore = 20f;
+                    table.SpacingAfter = 30f;
+
+                    addCell(table, "Emp No", 2);
+                    addCell(table, "Date", 2);
+                    addCell(table, "In Time", 2);
+                    addCell(table, "Late In", 2);
+                    addCell(table, "Early In", 2);
+                    addCell(table, "Out time", 2);
+                    addCell(table, "Early Out", 2);
+                    addCell(table, "Late out", 2);
+                    addCell(table, "Worked Time", 2);
+                    addCell(table, "Over Time", 2);
+                    addCell(table, "Status", 2);
+
+                    foreach (ReportRecord row in records)
+                    {
+                        int type = 1;
+                        if (row.Status == "Incorrect record")
+                        {
+                            type = 3;
+                        }
+                        addCell(table, row.EmployeeNumber.ToString(), type);
+                        addCell(table, row.DisplayRecordDate.ToString(), type);
+                        addCell(table, row.DisplayInTime, type);
+                        addCell(table, row.LateIn.ToString(), type);
+                        addCell(table, row.EarlyIn.ToString(), type);
+                        addCell(table, row.DisplayOutTime, type);
+                        addCell(table, row.EarlyOut.ToString(), type);
+                        addCell(table, row.LateOut.ToString(), type);
+                        addCell(table, row.WorkTime, type);
+                        addCell(table, row.OverTime, type);
+                        addCell(table, row.Status, type);
+                    }
+                    document.Add(table);
+
+                    document.Close();
+                    writer.Close();
+                    fs.Close();
+                    System.Diagnostics.Process.Start(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private static void addCell(PdfPTable table, string text, int type)
+        {
+            PdfPCell cell;
+            if (type == 1)
+            {
+                cell = new PdfPCell(new Phrase(text, FontFactory.GetFont("Tahoma", 10, Font.NORMAL)));
+            }
+            else if (type == 2)
+            {
+                cell = new PdfPCell(new Phrase(text, FontFactory.GetFont("Tahoma", 11, Font.BOLD)));
+                cell.BackgroundColor = BaseColor.GRAY;  
+            }
+            else
+            {
+               // BaseColor myColor = new BaseColor(00052);
+                cell = new PdfPCell(new Phrase(text, FontFactory.GetFont("Tahoma", 10, Font.NORMAL)));
+                cell.BackgroundColor = BaseColor.LIGHT_GRAY;  
+            }
+            cell.Rowspan = 1;
+            cell.HorizontalAlignment = PdfPCell.ALIGN_CENTER;
+            cell.VerticalAlignment = PdfPCell.ALIGN_MIDDLE;
+            table.AddCell(cell);
+        }
+
+
     }
 }
